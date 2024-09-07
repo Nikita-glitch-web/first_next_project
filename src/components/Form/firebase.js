663c8498db8848783703de196870cd0fd3fc7f3a
Nick <nikitalytvynov0506@gmail.com>
2024-09-07 15:47:57 +0300

    firebase-changes

--- src/components/Form/firebase.js
+++ src/components/Form/firebase.js
@@ -6,12 +6,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDg4h_s_roDB_PI5DZ2jSPcv1kuPuxneBQ",
+  authDomain: "first-next-project-1e8d7.firebaseapp.com",
+  projectId: "first-next-project-1e8d7",
+  storageBucket: "first-next-project-1e8d7.appspot.com",
+  messagingSenderId: "623502899574",
+  appId: "1:623502899574:web:783ce9677ca2a28e166257",
+  measurementId: "G-904RMYV5WV",
 };
 
 const app = initializeApp(firebaseConfig);
